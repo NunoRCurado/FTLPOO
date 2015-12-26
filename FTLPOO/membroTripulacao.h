39df1c8bf688d3b7d0060a927678c97ae9ea13eb
AdriDelegado <adrielpt91@gmail.com>
2015-12-26 22:08:15 +0000

    v1.002
    inclusao da classe membroTripulação

---
+++ FTLPOO/membroTripulacao.h
@@ -0,0 +1,34 @@
+#pragma once
+#ifndef MEMBROTRIPULACAO_H
+#define MEMBROTRIPULACAO_H
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class membroTripulacao{
+	int repirar;
+	int reparador;
+	int combatente;
+	string tripulacao;
+public:
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
